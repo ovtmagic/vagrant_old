4fe62fbbb9d2bbf97816f3bc96eb8401f527a7f8
Octavio Vicente <ovtmagic@gmail.com>
2019-11-29 13:37:00 +0100

    Downgrade of minikube an k8s
    Last Helm version is not working with K8s 1.6.2. Therefore I've made a downgrade of:
    - minikube to version 1.0.0
    - kubectl to version 1.4.9

--- vagrant_minikube/install.sh
+++ vagrant_minikube/install.sh
@@ -1,6 +1,6 @@
 # apps
 echo "set -g mouse" > /home/vagrant/.tmux.conf
-#apt-get install -y aptitude git socat
+apt-get install -y aptitude git socat
 
 # Docker
 sudo apt-get update
@@ -27,11 +27,12 @@ sudo apt-get update && sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubectl
+sudo apt-get install -y kubectl=1.14.2-00
 
 
 # Minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /dev/null 2>&1
+#curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 > /dev/null 2>&1
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.1.0/minikube-linux-amd64 > /dev/null 2>&1
 chmod +x minikube
 sudo cp minikube /usr/local/bin && rm minikube
 
